9d5a822afb09b1f435101772cbcb36799b1404b2
zhuxx <zhuxiaoxia0215@qq.com>
2024-03-30 21:06:29 +0800

    1.minio 封装为starter使用

--- heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
@@ -1,13 +1,24 @@
 package com.heima.minio.test;
 
+import com.heima.file.service.FileStorageService;
+import com.heima.minio.MinIOApplication;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-
+@SpringBootTest(classes = MinIOApplication.class)
+@RunWith(SpringRunner.class)
 public class MinIOTest {
 
+    @Autowired
+    private FileStorageService fileStorageService;
 
     public static void main(String[] args) {
 
@@ -25,5 +36,13 @@ public class MinIOTest {
 
     }
 
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("D:/list.html");
+        String path = fileStorageService.uploadHtmlFile("", "list.html", fileInputStream);
+        System.out.println(path);
+
+    }
 
 }
