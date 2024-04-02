8af1feef66c18455d3bffc3556d4c150080e68a0
zhuxx <zhuxiaoxia0215@qq.com>
2024-04-02 21:30:49 +0800

    1.阿里云内容安全识别接口

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedia/test/AliyunTest.java
@@ -0,0 +1,23 @@
+package com.heima.wemedia.test;
+
+import com.heima.common.aliyun.GreenTextScan;
+import com.heima.wemedia.WemediaApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = WemediaApplication.class)
+@RunWith(SpringRunner.class)
+public class AliyunTest {
+
+    @Autowired
+    private GreenTextScan greenTextScan;
+
+    @Test
+    public void testScan() throws Exception {
+        String content = "你妈死了";
+        System.out.println(greenTextScan.greeTextScan(content));
+    }
+}
